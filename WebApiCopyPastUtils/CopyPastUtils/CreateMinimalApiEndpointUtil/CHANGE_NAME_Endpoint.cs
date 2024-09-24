a37e7266febe914a12b533bbe188230578d98db0
Kovalenko Vitaliy Michailovich <81772795+Pragrammist@users.noreply.github.com>
2024-09-24 09:15:33 +0300

    Первые утилиты

---
+++ WebApiCopyPastUtils/CopyPastUtils/CreateMinimalApiEndpointUtil/CHANGE_NAME_Endpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApiCopyPastUtils.CopyPastUtils.CreateMinimalApiEndpointUtil
+{
+    /// <summary>
+    /// Создание эндпоинт для minimal api
+    /// #minimal-api #endpoint
+    /// </summary>
+
+    public static class CHANGE_NAME_Endpoint
+    {
+        public static IEndpointConventionBuilder CHANGE_NAME_(this IEndpointRouteBuilder builder)
+        {
+            return builder.MapGet("CHANGE_NAME", () => 
+            {
+
+            });
+        }
+    }
+}
---
+++ WebApiCopyPastUtils/CopyPastUtils/SqlConnectionFactory/SqlConnectionFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.Common;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebApiCopyPastUtils.CopyPastUtils.SqlConnectionFactory
+{
+    public interface ISqlConnectionFactory : IDisposable, IAsyncDisposable
+    {
+        ValueTask<IDbConnection> StartQuery();
+    }
+    class SqlConnectionFactory(IConfiguration configuration) : IDisposable, IAsyncDisposable, ISqlConnectionFactory
+    {
+        private DbConnection _connection = null!;
+        private bool _disposedValue;
+        private bool disposedValue;
+
+        public async ValueTask<IDbConnection> StartQuery()
+        {
+            if (_connection is not null)
+                return _connection;
+
+            var connectionString = configuration.GetConnectionString("mssql");
+            _connection = new SqlConnection(connectionString);
+            await _connection.OpenAsync();
+            return _connection;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _connection?.Dispose();
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~SqlConnectionFactory()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (!disposedValue)
+            {
+                if (_connection is not null)
+                    await _connection.DisposeAsync();
+            }
+        }
+    }
+}
