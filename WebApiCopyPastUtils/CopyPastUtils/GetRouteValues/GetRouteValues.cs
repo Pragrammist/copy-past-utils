a8c0193fe4189cb9763cda32af396a3952938aad
Kovalenko Vitaliy Michailovich <81772795+Pragrammist@users.noreply.github.com>
2024-09-26 18:15:33 +0300

    Роуты, Свагер

---
+++ WebApiCopyPastUtils/CopyPastUtils/GetRouteValues/GetRouteValues.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApiCopyPastUtils.CopyPastUtils.GetRouteValues
+{
+    //#minimal api #controllers #mvc
+    public static class GetRouteValues
+    {
+        /// <summary>
+        /// Возвращает в виде словаря параметры из маршрута
+        /// </summary>
+        /// <param name="httpContext">Контекст текущего запроса</param>
+        /// <returns></returns>
+        public static IDictionary<string, string> GetBadrequestParameters(HttpContext httpContext)
+        {
+            var result = httpContext.Request.RouteValues
+                .Select(p => new KeyValuePair<string, string>
+                    (p.Key, p.Value?.ToString() ?? string.Empty))
+                //Нужно чтобы убрать лишние параметры
+                .Where(p => p.Key != "action" && p.Key != "controller")
+                .ToDictionary();
+            return result;
+        }
+
+
+        /// <summary>
+        /// Возвращает в виде словаря параметры из маршрута
+        /// </summary>
+        /// <param name="httpContext">Контекст текущего запроса</param>
+        /// <param name="parametersName">Параметры для текущего запроса</param>
+        /// <returns></returns>
+        public static IDictionary<string, string> GetBadrequestParameters(HttpContext httpContext, params string[] parametersName)
+        {
+            var result = httpContext.Request.RouteValues
+                .Select(p => new KeyValuePair<string, string>
+                    (p.Key, p.Value?.ToString() ?? string.Empty))
+                //Нужно чтобы убрать лишние параметры
+                .Where(p => parametersName.Contains(p.Key))
+                .ToDictionary();
+            return result;
+        }
+    }
+}
---
+++ WebApiCopyPastUtils/CopyPastUtils/SwaggerConfiguration/SwaggerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiCopyPastUtils.CopyPastUtils.SwaggerConfiguration
+{
+    //#swagger #configuration
+    public static class SwaggerConfiguration
+    {
+        
+        // Сервисы
+        // builder.Services.AddEndpointsApiExplorer();
+        // builder.Services.AddSwaggerGen();
+
+        // Конфигурация http pipeline.
+        //app.UseSwagger();
+        //app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+        //{
+        //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        //    options.RoutePrefix = "api/swagger"; 
+        //    //В Properties/launchSettings.json в profiles.<profile_name>.launchUrl поставить "api/swagger"
+        //});
+    }
+}
